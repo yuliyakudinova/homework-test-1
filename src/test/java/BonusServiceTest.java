a00f316a33d48ab18d975614ec5c9431e36b622a
yulechka.kudinova@mail.ru <yulechka.kudinova.1991@mail.ru>
2022-01-21 14:47:46 +0300

    second commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,53 +1,13 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    void shouldCalculate(String testName, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
-
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
-
-        long actual = service.calculate(amount, registered);
-
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
-
-        long amount = 1_000_000_60;
-        boolean registered = true;
-        long expected = 500;
-
-        long actual = service.calculate(amount, registered);
-
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForNotRegisteredAndUnderLimit() {
-        BonusService service = new BonusService();
-
-        long amount = 1000_60;
-        boolean registered = false;
-        long expected = 10;
-
-        long actual = service.calculate(amount, registered);
-
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForNotRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
-
-        long amount = 1_000_000_60;
-        boolean registered = false;
-        long expected = 500;
 
         long actual = service.calculate(amount, registered);
 
